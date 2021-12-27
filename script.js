797a1ff5b38acd581cd581d57eca02e41d6d84df
Nikhil Kumar <nikhil.kumar.18@mountblue.tech>
2021-12-27 11:01:37 +0530

    modifies code

--- script.js
+++ script.js
@@ -1,50 +1,64 @@
 function priceChange() {
 
-    let basic = document.getElementById("basic-price")
-    let prof = document.getElementById("prof-price")
-    let master = document.getElementById("master-price")
-
-    fetch("./price.json")
-    .then((res)=>{
+  fetch("./price.json")
+    .then((res) => {
       return res.json()
     })
-    .then((data)=>{
-      const check=document.querySelector('input')
-      if(check.checked){
-        
-        basic.innerHTML = `<span class="dollar">&dollar;</span>${data.monthly.basic}`;
-        prof.innerHTML = `<span class="dollar">&dollar;</span>${data.monthly.prof}`;
-        master.innerHTML = `<span class="dollar">&dollar;</span>${data.monthly.master}`;
-      }
-      else{
-        basic.innerHTML = `<span class="dollar">&dollar;</span>${data.annual.basic}`;
-        prof.innerHTML = `<span class="dollar">&dollar;</span>${data.annual.prof}`;
-        master.innerHTML = `<span class="dollar">&dollar;</span>${data.annual.master}`;
-      }
+    .then((data) => {
+      addPrice(data)
     })
-    .catch((err)=>{
+    .catch((err) => {
       return console.error(err)
     })
 
-    // if (basic.innerHTML == '<span class="dollar">$</span>199.99') {
+}
 
-    //   basic.innerHTML = '<span class="dollar">&dollar;</span>19.99';
-    // }
-    // else {
-    //   basic.innerHTML = '<span class="dollar">&dollar;</span>199.99';
-    // }
-    // if (prof.innerHTML == '<span class="dollar">$</span>249.99') {
-    //   prof.innerHTML = '<span class="dollar">&dollar;</span>24.99';
-    // }
-    // else {
-    //   prof.innerHTML = '<span class="dollar">&dollar;</span>249.99'
-    // }
+function addPrice(data) {
+  
+  let basic = document.getElementById("basic-price")
+  
+  let prof = document.getElementById("prof-price")
+  
+  let master = document.getElementById("master-price")
+  
+  const check = document.querySelector('input')
+  
+  const dollar = document.createElement('span')
+  
+  dollar.className = 'dollar';
+  
+  dollar.append('$')
+  
+  addDollar(basic, prof, master, dollar)
 
-    // if (master.innerHTML == '<span class="dollar">$</span>399.99') {
-    //   master.innerHTML = '<span class="dollar">&dollar;</span>39.99';
-    // }
-    // else {
-    //   master.innerHTML = '<span class="dollar">&dollar;</span>399.99'
-    // }
+
+  if (check.checked) {
+
+    basic.append(`${data.monthly.basic}`)
+
+    prof.append(`${data.monthly.prof}`);
     
+    master.append(`${data.monthly.master}`);
   }
+  else {
+    basic.append(`${data.annual.basic}`)
+
+    prof.append(`${data.annual.prof}`);
+    
+    master.append(`${data.annual.master}`);
+  }
+}
+
+function addDollar(basic, prof, master, dollar) {
+  basic.innerHTML = ''
+  
+  basic.appendChild(dollar.cloneNode(true))
+  
+  prof.innerHTML = ''
+  
+  prof.appendChild(dollar.cloneNode(true))
+  
+  master.innerHTML = ''
+  
+  master.appendChild(dollar.cloneNode(true))
+}
